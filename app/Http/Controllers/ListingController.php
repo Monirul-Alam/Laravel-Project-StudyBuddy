afa9015082dfcb3ab1fffa57afb752ccfec81585
Monirul Alam <you@example.com monirulcr2325@gmail.com>
2022-06-30 03:00:54 +0600

    Completed Landing User Page, Search Operation, Pagination and CRUD Operation

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+     public function index() {
+        return view('listings.index',[
+            'heading' => 'latest listing',
+         'listings'=> Listing::latest()->filter(request(['tag', 'search']))->simplepaginate(6)
+     ]); 
+
+     }
+
+
+     public function show(Listing $listing) {
+        return view('listings.show',[
+            'listing' => $listing
+        ]);
+     }
+     // Show create form
+     public function create() { 
+       return view('listings.create');
+     }
+
+     // Store listing data
+     public function store(Request $request) { 
+        // dd($request->file('logo'));
+        $formFields =$request->validate(
+            [
+                'title' => 'required',
+                'company' => ['required', Rule::unique('listings','company')],
+                'location' => 'required',
+                'website' => 'required',
+                'email' => ['required', 'email'],
+                'tags' => 'required',
+                'description' => 'required'
+
+            ]); 
+
+            if($request->hasfile('logo')) {
+                $formFields['logo'] = $request->file('logo')->store('logos','public');
+            }
+
+            Listing::create($formFields);
+            return redirect('/')->with('message', 'Listing Created Sucessfully');
+      }
+      // Show Edit form
+      public function edit(Listing $listing) {
+        return view('listings.edit',['listing' => $listing]);
+      }
+      // Updating  listing data
+     public function update(Request $request , Listing $listing) { 
+        // dd($request->file('logo'));
+        $formFields =$request->validate(
+            [
+                'title' => 'required',
+                'company' => ['required'],
+                'location' => 'required',
+                'website' => 'required',
+                'email' => ['required', 'email'],
+                'tags' => 'required',
+                'description' => 'required'
+
+            ]); 
+
+            if($request->hasfile('logo')) {
+                $formFields['logo'] = $request->file('logo')->store('logos','public');
+            }
+
+            $listing->update($formFields);
+            return back()->with('message', 'Listing updated Sucessfully');
+      }
+
+      // Delete Listing
+      public function destroy(Listing $listing) {
+        $listing->delete();
+        return redirect('/')->with('message','Listing Delated Successfully');
+
+      }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+
+// Store Listing Data
+Route::post('/listings/', [ListingController::class, 'store']);
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+// Edit submit to update
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
+
+// Single Listings
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
